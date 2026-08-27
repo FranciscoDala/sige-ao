065114ca9cf11fd6c070920bec1dd44579958d0a
Francisco Dala <killerbless12@gmail.com>
2026-08-27 13:45:25 +0100

    build frontend

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>SIGE AO</h1>
+      <p>Frontend no ar 🚀</p>
+    </div>
+  )
+}
+
+export default App
