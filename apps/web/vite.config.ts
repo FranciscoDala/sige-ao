0b05d921de6b4a4cf6eb525df7f22549c5fbfab9
Francisco Dala <killerbless12@gmail.com>
2026-09-02 19:11:16 +0100

    update: ajuste do vite.config.ts corrigido  02/09/26, 19:11m

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/', // <- 1. Importante pra SPA no Render
+    build: {
+        outDir: 'dist', // <- 2. Pasta que o Render vai servir
+        emptyOutDir: true,
+    },
+    server: {
+        port: 5173,
+    }
 })
